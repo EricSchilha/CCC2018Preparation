5aed998c0744599e0febc3436790c6fe40c98709
EricSchilha <eschilha@gmail.com>
2017-12-17 16:41:44 -0500

    Update S22017.java

--- src/S22017.java
+++ src/S22017.java
@@ -1,39 +1,28 @@
-
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.Scanner;
 
 public class S22017 {
-
-    public static void main(String[] args) {
+    public static void main(String [] args) {
         Scanner sin = new Scanner(System.in);
-        int tideCount = sin.nextInt();
-        int tides[] = new int[tideCount], lowTides[] = new int[(int) Math.ceil(tideCount / 2)], highTides[] = new int[(int) Math.floor(tideCount / 2)];
-        for (int i = 0; i < tideCount; i++) {
+        double tideCount = (int)sin.nextDouble();
+        int tides[] = new int[(int)tideCount], lowTides[] = new int[(int)Math.ceil(tideCount/2)], highTides[] = new int[(int)Math.floor(tideCount/2)];
+        for(int i = 0; i < tideCount; i++){
             tides[i] = sin.nextInt();
         }
         Arrays.sort(tides);
-        for (int i = 0; i < lowTides.length; i++) {
+        for(int i = 0; i < lowTides.length; i++){
             lowTides[i] = tides[i];
         }
-        for (int i = 0; i < highTides.length; i++) {
-            highTides[i] = tides[i + lowTides.length];
+        for(int i = 0; i < highTides.length; i++){
+            highTides[i] = tides[i+lowTides.length];
         }
-        for (int i = 0; i < lowTides.length/2; i++) {
-            int temp = lowTides[i];
-            lowTides[i] = lowTides[lowTides.length - 1 - i];
-            lowTides[lowTides.length - 1 - i] = temp;
-        }
-        for (int i = 0; i < lowTides.length; i++) {
-            System.out.print(lowTides[i] + " ");
-            /*if(!(i==highTides.length-1 && lowTides.length>highTides.length))
-             System.out.print(highTides[i] + " ");*/
-        }
-        System.out.println("");
-        for (int i = 0; i < highTides.length; i++) {
-            System.out.print(highTides[i] + " ");
-            /*if(!(i==highTides.length-1 && lowTides.length>highTides.length))
-             System.out.print(highTides[i] + " ");*/
+        Arrays.sort(highTides);
+        Arrays.sort(lowTides);
+        for(int i = 0; i < lowTides.length; i++){
+            System.out.print(lowTides[lowTides.length-i-1] + " ");
+            if(i != highTides.length || lowTides.length == highTides.length) {
+                System.out.print(highTides[i] + " ");
+            }
         }
     }
 }

be7683f901cb3701bb1722387f1ba58f44369825
schie3227 <schie3227@FHC-IC0026757.wrdsb.ca>
2017-12-19 14:30:50 -0500

    1

--- src/J32016.java
+++ src/J32016.java
@@ -1,23 +1,26 @@
 
 import java.util.Scanner;
-
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/**
- *
- * @author schie3227
- */
 public class J32016 {
-
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         Scanner sin = new Scanner(System.in);
         String sWord = sin.next();
-        
+        boolean done = false;
+        int nMPL = sWord.length();//max possible length
+        int nMaxOffset = 0, nAns = 1;
+        while(!done){
+            nMaxOffset = sWord.length()-nMPL;
+            for(int i = 0; i < nMaxOffset; i++){
+                done = isPalindrome(sWord.substring(i, nMPL-nMaxOffset+i));
+                if(done){
+                    nAns = nMPL;
+                    break;
+                }
+            }
+            nMPL--;
+        }
+    }
+    
+    public static boolean isPalindrome(String sTest){
+        return false;
     }
 }
